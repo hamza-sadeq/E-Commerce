ad440af97af32032ebb95f8bf197cab3652a7d38
hamza-sadeq <hamzasadeq41@gmail.com>
2023-03-22 06:38:10 +0200

    E-Commerce

---
+++ main.js
@@ -0,0 +1,7 @@
+const openSidebar = ()=>{
+    document.getElementById("sidebar").style.right = '0'
+} 
+
+const closeSidebar = ()=>{
+    document.getElementById("sidebar").style.right = '-280px'
+} 
